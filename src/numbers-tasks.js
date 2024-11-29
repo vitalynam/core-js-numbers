a27ac54fa8b7e70c41548541c548251f7548eb35
Namiko01 <vitaliknam01@gmail.com>
2024-11-30 02:24:08 +0300

    feat: pass 23

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -84,8 +84,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
@@ -105,8 +105,16 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  // Скалярное произведение векторов
+  const dotProduct = x1 * x2 + y1 * y2;
+  // Длины векторов
+  const lengthA = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const lengthB = Math.sqrt(x2 ** 2 + y2 ** 2);
+  // Косинус угла
+  const cosTheta = dotProduct / (lengthA * lengthB);
+  // Арккосинус для получения угла
+  return Math.acos(cosTheta);
 }
 
 /**
